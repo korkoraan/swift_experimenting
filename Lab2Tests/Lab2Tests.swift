3d91129dec0f84518273b088e38062d6fba9fc04
korkoraan <hemul888@gmail.com>
2023-12-17 16:28:54 +0600

    init

---
+++ Lab2Tests/Lab2Tests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import Lab2
+
+final class Lab2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
